cd66c8c160b548d7194d94e66b93a45c8510b2ca
Adam Green <adam.green@adgefficiency.com>
2020-01-14 10:49:59 +0100

    initial commit - hello world

---
+++ setup.sh
@@ -0,0 +1,4 @@
+kaggle competitions download -c data-science-bowl-2019
+mkdir $HOME/ds-bowl-from-scratch
+mv *.zip $HOME/ds-bowl-from-scratch
+unzip $HOME/ds-bowl-from-scratch/*.zip -d $HOME/ds-bowl-from-scratch/raw-data
